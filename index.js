fc6ced0e8a0d5927b28e05bb655e41c448db040d
Sokolinyy <kostyk.vasilii@gmail.com>
2022-09-02 00:30:37 +0200

    FInishing design and add some changes to function

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ function rotate(element, rotation) { //
 }
 
 
-function infoButtonRender(button_id, div_id, img_id) {
+function infoButtonRender(button_id, div_id, img_id, text_button_id) {
   document.getElementById(button_id).addEventListener("click", function() {
     info = document.getElementById(div_id)
+    textButton = document.getElementById(text_button_id)
     if (info.style.display == "none") {
       enable = true
       rotate(img_id, 180)
       info.style.display = "block";
+      textButton.style.fontWeight = "600";
     }
     else {
       enable1 = true
       rotate(img_id, 0)
       info.style.display = "none";
+      textButton.style.fontWeight = "400"
     }
   });
 }
-  infoButtonRender("button1", "info1", "#arrow-icon1")
-  infoButtonRender("button2", "info2", "#arrow-icon2")
-  infoButtonRender("button3", "info3", "#arrow-icon3")
-  infoButtonRender("button4", "info4", "#arrow-icon4")
-  infoButtonRender("button5", "info5", "#arrow-icon5")
+  infoButtonRender("button1", "info1", "#arrow-icon1", "textButton1")
+  infoButtonRender("button2", "info2", "#arrow-icon2", "textButton2")
+  infoButtonRender("button3", "info3", "#arrow-icon3", "textButton3")
+  infoButtonRender("button4", "info4", "#arrow-icon4", "textButton4")
+  infoButtonRender("button5", "info5", "#arrow-icon5", "textButton5")
